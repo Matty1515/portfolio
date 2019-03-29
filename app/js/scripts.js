557f9cd19a65ebd89b7840de654c115bee2f94a0
Matthew Shaw <imhullu15@gmail.com>
2019-03-29 16:30:28 +0000

    animations and text completed

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,6 +1,21 @@
 /* jshint esversion: 6 */
 $(document).ready(() => {
-	
-console.log('JavaScript is working!');
 
+const animateOnScroll = function(scrollDiv, scrollTarget) {
+	$(window).scroll(function() {
+	   var hT = $(scrollDiv).offset().top,
+	       hH = $(scrollDiv).outerHeight(),
+	       wH = $(window).height(),
+	       min = 10,
+	       wS = $(this).scrollTop();
+	   if (wS > (hT+hH-wH-min)){
+	    $(scrollTarget).fadeIn(3000);
+	   }
+	});
+};
+animateOnScroll('#err', '#err-fade-in');
+animateOnScroll('#gmc', '#gmc-fade-in');
+animateOnScroll('#tsm', '#tsm-fade-in');
+
+console.log('JavaScript is working!');
 });
