f039257b135f14a074bcbfbb71aca8d1c77684b2
Matthew Shaw <imhullu15@gmail.com>
2019-04-03 17:06:32 +0100

    completed scrollbar functionality

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,15 +1,49 @@
 /* jshint esversion: 6 */
 $(document).ready(() => {
 
+// identifies which page the user is looking at and scrolls up/down to the next one
+let buttonScroll = function() {
+	$(window).scroll(function() {
+		currentPosition = $(this).scrollTop();
+		errHeight = $('#err').offset().top;
+		gmcHeight = $('#gmc').offset().top;
+		tsmHeight = $('#tsm').offset().top;
+		if (currentPosition >= errHeight && currentPosition < gmcHeight) {
+			$('#scrollUp').on('click', () => {
+				window.scrollTo(0, 0);
+			});
+			$('#scrollDown').on('click', () => {
+				window.scrollTo(0, $('#gmc').offset().top + 5);
+			});
+		} else if (currentPosition >= gmcHeight && currentPosition < tsmHeight) {
+			$('#scrollUp').on('click', () => {
+				window.scrollTo(0, $('#err').offset().top + 5);
+			});
+			$('#scrollDown').on('click', () => {
+				window.scrollTo(0, $('#tsm').offset().top + 5);
+			});
+		} else if (currentPosition >= tsmHeight) {
+			$('#scrollUp').on('click', () => {
+				window.scrollTo(0, $('#gmc').offset().top + 5);
+			});
+			$('#scrollDown').on('click', () => {
+				window.scrollTo(0, $('#tsm').offset().top + 5);
+			});
+		}
+	});
+};
+buttonScroll();
+
+// animates the website screenshot images when the parent page is scrolled to
 const animateOnScroll = function(scrollDiv, scrollTarget) {
 	$(window).scroll(function() {
 	   var hT = $(scrollDiv).offset().top,
 	       hH = $(scrollDiv).outerHeight(),
 	       wH = $(window).height(),
-	       min = 10,
+	       min = 50,
 	       wS = $(this).scrollTop();
 	   if (wS > (hT+hH-wH-min)){
-	    $(scrollTarget).fadeIn(3000);
+	   	$(scrollTarget).fadeIn(3000);
 	   }
 	});
 };
@@ -17,5 +51,4 @@ const animateOnScroll = function(scrollDiv, scrollTarget) {
 animateOnScroll('#gmc', '#gmc-fade-in');
 animateOnScroll('#tsm', '#tsm-fade-in');
 
-console.log('JavaScript is working!');
 });
